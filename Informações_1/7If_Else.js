9f87e2b3ccf7b125a252e0336f73f93dc3ae3036
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-11-01 13:56:16 -0300

    .

---
+++ Informações_1/7If_Else.js
@@ -0,0 +1,8 @@
+var a = 12;
+var b = 5;
+if (a == b) {
+console.log("12 é igual a 5?!?!");
+} else {
+console.log("a é diferente de b");
+};
+// No caso acima a frase escrita seria "a é diferente de b"
