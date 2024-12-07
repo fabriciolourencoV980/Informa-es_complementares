4949fab9f9dd6b0c57f8caecb2b5be243db26031
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-12-07 08:57:43 -0300

    .

---
+++ IlustraCod_Programation_3/Array_multi.js
@@ -0,0 +1,12 @@
+let n = {
+    semanas: {
+        1: "Domingo",
+        2: "Segunda"
+    },
+    meses: {
+        1: "Janeiro",
+        2: "Fevereiro",
+    }
+};
+
+document.write(n["semanas"][1]+' '+ n["meses"][2]);
---
+++ IlustraCod_Programation_4/Função.js
@@ -0,0 +1,6 @@
+function somar(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let soma = parseFloat(n1) + parseFloat(n2);
+    alert(soma); 
+};
